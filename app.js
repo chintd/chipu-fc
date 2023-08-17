75c8ce0a2b23b3021d74ace928aad46081f1f344
chintd <119962869+chintd@users.noreply.github.com>
2023-08-17 18:23:26 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
-const express = require("express");
-const shopRouter = require("./router/main");
-const userRouter = require("./router/user")
-const app = express();
-const cors = require('cors');
-app.use(cors());
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.json());
-app.use(shopRouter);
-app.use("/user", userRouter)
-app.listen(5000);
